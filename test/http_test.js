08de22cc468e8c84fe88dd163faa3b96ff1aef43
Kristoffer Walker <kris@kixx.name>
2014-02-11 12:19:08 -0500

    Refactor HTTP testing.
     On branch master
    	modified:   lib/http.js
    	modified:   test/http_test.js

--- test/http_test.js
+++ test/http_test.js
@@ -1,7 +1,42 @@
-var HTTP = require('../lib/').HTTP
+var LIB = require('../lib/')
+  , HTTP = LIB.HTTP
 
-  , Request = require('request').Request
+  , REQ = require('request')
+  , Request = REQ.Request
 
+
+exports["get() without error"] = {
+  setUp: function (done) {
+    this.originalMethod = REQ.get;
+    var options = this.options = Object.create(null)
+      , body = this.body = Object.create(null)
+      , response = this.response = Object.create(null)
+      , request = this.request = Object.create(null)
+
+    REQ.get = function (opts) {
+      LIB.extend(options, opts);
+      process.nextTick(function () {
+        return options.callback(null, response, body);
+      });
+      return request;
+    };
+
+    return done();
+  },
+
+  tearDown: function (done) {
+    REQ.get = this.originalMethod;
+    return done();
+  },
+
+  "passes uri": function (test) {
+    test.expect(1);
+    HTTP.get('http://www.example.com').promise().then(function (res) {
+      return test.done();
+    });
+    test.equal(this.options.uri, 'http://www.example.com');
+  }
+};
 
 exports["with GET request"] = {
   setUp: function (done) {
@@ -21,7 +56,12 @@ exports["with GET request"] = {
     return test.done();
   },
 
-  "it has response headers": function (test) {
+  "its status is 200": function (test) {
+    test.strictEqual(this.res.statusCode, 200);
+    return test.done();
+  },
+
+  "its content-type is text/html": function (test) {
     var headers = this.res.headers
     test.equal(headers['content-type'], 'text/html');
     return test.done();
