3bd88efdb1f75a47c5e442cc0fc726e9a15c192c
Kristoffer Walker <kris@kixx.name>
2015-04-07 13:50:28 -0400

    Running jshint
    	new file:   .jshintignore
    	new file:   .jshintrc
    	new file:   management/run_jshint.js
    	modified:   package.json

---
+++ management/run_jshint.js
@@ -0,0 +1,28 @@
+var
+CP       = require('child_process'),
+FilePath = require('filepath').FilePath;
+
+function main() {
+  var
+  projectDir = FilePath.create(__dirname).dirname();
+  spawn({
+    jshint: projectDir.append('node_modules', '.bin', 'jshint'),
+    target: projectDir
+  });
+}
+
+function spawn(args) {
+  var
+  argv = [args.jshint.toString(), args.target.toString()],
+  child = CP.spawn('node', argv);
+  child.on('error', function (err) {
+    console.error('JSHint process error:');
+    console.error(err.stack || err.message);
+    process.exit(1);
+  });
+  child.stdout.pipe(process.stdout);
+  child.stderr.pipe(process.stderr);
+}
+
+main();
+
