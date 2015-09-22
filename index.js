3a2cdb750acdb533bcb377cc328bcb14465393e0
Kristoffer Walker <kris@kixx.name>
2015-09-21 20:13:14 -0400

    Change exports.FP to exports.filepath
    	modified:   index.js

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 'use strict';
 
 var
-Promise = require('./lib/promise'),
-U       = require('./lib/u'),
-objects = require('./lib/objects'),
-FP      = require('filepath'),
-errors  = require('./lib/errors'),
-REQ     = require('./lib/http'),
+Promise  = require('./lib/promise'),
+U        = require('./lib/u'),
+objects  = require('./lib/objects'),
+filepath = require('filepath'),
+errors   = require('./lib/errors'),
+REQ      = require('./lib/http'),
 
 applicationLoader = require('./lib/application_loader');
 
 
-exports.Promise = Promise;
-exports.U       = U;
-exports.objects = objects;
-exports.FP      = FP;
-exports.errors  = errors;
-exports.REQ     = REQ;
+exports.Promise  = Promise;
+exports.U        = U;
+exports.objects  = objects;
+exports.filepath = filepath;
+exports.errors   = errors;
+exports.REQ      = REQ;
 
 
 exports.load = function (args, callback) {
   args = args || Object.create(null);
 
   var promise = applicationLoader.load({
-    appdir       : args.appdir || FP.create().resolve(process.argv[1]).dirname(),
+    appdir       : args.appdir || filepath.create().resolve(process.argv[1]).dirname(),
     name         : args.name,
     version      : args.version,
     usageString  : args.usageString,
