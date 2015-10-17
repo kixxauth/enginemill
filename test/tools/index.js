5b48b31e50c8da7db1ac7903da27ff475c8d6209
Kristoffer Walker <kris@kixx.name>
2015-10-17 06:30:52 -0400

    Adds fake GUID generator to test tools.
    	modified:   test/tools/index.js

--- test/tools/index.js
+++ test/tools/index.js
@@ -9,6 +9,16 @@ exports.hasOwnProp = function (obj, key) {
   return Object.prototype.hasOwnProperty.call(obj, key);
 };
 
+exports.fake_guid = function () {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
+};
+
 exports.runOnce = (function () {
   var results = Object.create(null);
   return function (key, fn) {
