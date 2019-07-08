cc31edb107e3f861800015d42beb0b796dbb62d3
Kris Walker <kris@kixx.name>
2019-07-08 11:04:02 -0400

    Running example (fails, but it runs to failure ;-] )

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const Filepath = require('./lib/filepath');
+const Enginemill = require('./lib/enginemill');
+
+
+exports.buildAllResources = function () {
+	const eng = Enginemill.create();
+
+	const options = {
+		baseDirectory: Filepath.create(process.cwd())
+	};
+
+	/* eslint-disable no-console */
+	eng.initialize(options).buildAllResources().then((res) => {
+		console.log('DONE:');
+		console.log(res);
+	}).catch((err) => {
+		console.log('Runtime ERROR:');
+		console.log(err.stack || err);
+	});
+	/* eslint-enable no-console */
+};
