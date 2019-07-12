c70cc3c85e63a355a826831d00e6413f89713a3a
Kris Walker <kris@kixx.name>
2019-07-12 09:02:23 -0400

    Verifyig ResourcesDB

--- index.js
+++ index.js
@@ -26,3 +26,27 @@ exports.buildAllResources = function () {
 		});
 	/* eslint-enable no-console */
 };
+
+
+exports.validateResources = function () {
+	const eng = Enginemill.create();
+
+	const options = {
+		baseDirectory: Filepath.create(process.cwd())
+	};
+
+	/* eslint-disable no-console */
+	eng.initialize(options)
+		.then(() => {
+			return eng.validateResources();
+		})
+		.then((res) => {
+			console.log('DONE:');
+			console.log(res);
+		})
+		.catch((err) => {
+			console.log('Runtime ERROR:');
+			console.log(err.stack || err);
+		});
+	/* eslint-enable no-console */
+};
